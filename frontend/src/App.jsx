ac14a8e49005c7b66d7c2db2a6b8098eb971e5d9
abhishek <abhishek.nt94@gmail.com>
2023-06-05 23:21:34 -0700

    Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/header'
+import HomeScreen from './Screens/HomeScreen'
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+      <HomeScreen/>
+     
+    </div>
+  )
+}
+
+export default App
