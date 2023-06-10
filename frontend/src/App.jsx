1da0717648631632b8ed5251479e927025d5524e
abhishek <abhishek.nt94@gmail.com>
2023-06-10 10:10:35 -0700

    added redux stor,screens and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './components/header'
-import HomeScreen from './Screens/HomeScreen'
+import { Outlet } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
       <Header/>
-      <HomeScreen/>
+      <ToastContainer/>
+      <Container className='my-2'>
+      <Outlet/>
+      </Container>
      
     </div>
   )
--- frontend/src/Screens/HomeScreen.jsx
+++ frontend/src/Screens/HomeScreen.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import Hero from '../components/hero'
+
 
 
 const HomeScreen = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Hero/>
   )
 }
 
